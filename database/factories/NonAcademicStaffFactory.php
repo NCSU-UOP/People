5fd55720404863502b787ed8cc2f0638a6fd08ea
Nadun Welikanda <nadunwelikanda.360@gmail.com>
2022-03-20 11:18:49 +0530

    Non-Academic Staff seeder has been developed

--- database/factories/NonAcademicStaffFactory.php
+++ database/factories/NonAcademicStaffFactory.php
@@ -11,13 +11,32 @@ class NonAcademicStaffFactory extends Factory
 {
     /**
      * Define the model's default state.
-     *
      * @return array<string, mixed>
      */
     public function definition()
     {
+        $Gender = array_rand(['male', 'female']);
+        $firstName = $this->faker->firstName($gender = $Gender, $maxNbChars = 20);
+        $middleName = $this->faker->firstName($gender = $Gender, $maxNbChars = 20);
+        $lastName = $this->faker->lastName($maxNbChars = 20);
+        $city = $this->faker->city($maxNbChars = 80);
+
         return [
-            //
+            'employee_id' => $this->faker->lexify('??').$this->faker->unique()->numberBetween(20, 9000),
+
+            'fname' => $firstName,
+
+            'lname' => $lastName,
+
+            'initial' => $firstName[0].'.'.$middleName[0].'. '.$lastName,
+
+            'fullname' => $firstName.' '.$middleName.' '.$lastName,
+
+            'city' => $city,
+
+            'address' => $this->faker->streetAddress().', '.$city.', '.$this->faker->state().' '.$this->faker->postcode(),
+
+            'image' => $this->faker->imageUrl($width = 400, $height = 400, 'cats'),
         ];
     }
 }
