89dc053c6e8c8091ac6df4836da3ed7df8766c51
chandula <e16061@eng.pdn.ac.lk>
2022-03-20 10:41:23 +0530

    add new edit functionality and modals

--- resources/views/people/profile.blade.php
+++ resources/views/people/profile.blade.php
@@ -1,6 +1,175 @@
 @extends('layouts.app')
 
 @section('content')
+<!-- -------------------------- Bio Modal ------------------------------------- -->
+<div class="modal fade" id="biostaticBackdrop" data-bs-backdrop="static" data-bs-keyboard="false" tabindex="-1" aria-labelledby="staticBackdropLabel" aria-hidden="true">
+  <div class="modal-dialog modal-dialog-centered">
+    <div class="modal-content">
+      <div class="modal-header">
+        <h5 class="modal-title" id="staticBackdropLabel">Edit Bio</h5>
+        <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
+      </div>
+      <div class="modal-body">
+        <form method="POST" action="#" enctype="multipart/form-data">
+            @csrf
+            <label for="bio" class="form-label">Bio</label>
+            <textarea class="form-control @error('bio') is-invalid @enderror" rows="10" aria-label="bio textarea" id="bio"></textarea>
+            @error('bio')
+                <span class="invalid-feedback" role="alert">
+                    <strong>{{ $message }}</strong>
+                </span>
+            @enderror
+            <div class="modal-footer">
+                <button type="button" class="btn btn-secondary" data-bs-dismiss="modal">Close</button>
+                <button type="submit" class="btn btn-primary">Save</button>
+            </div>    
+        </form>
+      </div>
+    </div>
+  </div>
+</div>
+<!-- -------------------------------------------------------------------------- -->
+
+<!-- -------------------------- Social Media Modal ------------------------------------- -->
+<div class="modal fade" id="socialstaticBackdrop" data-bs-backdrop="static" data-bs-keyboard="false" tabindex="-1" aria-labelledby="staticBackdropLabel" aria-hidden="true">
+  <div class="modal-dialog modal-dialog-centered">
+    <div class="modal-content">
+      <div class="modal-header">
+        <h5 class="modal-title" id="staticBackdropLabel">Edit Social Media</h5>
+        <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
+      </div>
+      <div class="modal-body">
+        <form method="POST" action="#" enctype="multipart/form-data">
+            @csrf
+            <div class="row mb-3">
+                <label for="cv" class="col-sm-2 col-form-label">CV</label>
+                <div class="col-sm-10">
+                <input type="text" class="form-control @error('cv') is-invalid @enderror" class="form-control" id="cv">
+                @error('cv')
+                    <span class="invalid-feedback" role="alert">
+                        <strong>{{ $message }}</strong>
+                    </span>
+                @enderror
+                </div>
+            </div>
+
+            <div class="row mb-3">
+                <label for="facebook" class="col-sm-2 col-form-label">Facebook</label>
+                <div class="col-sm-10">
+                <input type="text" class="form-control @error('facebook') is-invalid @enderror" class="form-control" id="facebook">
+                @error('facebook')
+                    <span class="invalid-feedback" role="alert">
+                        <strong>{{ $message }}</strong>
+                    </span>
+                @enderror
+                </div>
+            </div> 
+
+            <div class="row mb-3">
+                <label for="linkdin" class="col-sm-2 col-form-label">LinkedIn</label>
+                <div class="col-sm-10">
+                <input type="text" class="form-control @error('linkdin') is-invalid @enderror" class="form-control" id="linkdin">
+                @error('linkdin')
+                    <span class="invalid-feedback" role="alert">
+                        <strong>{{ $message }}</strong>
+                    </span>
+                @enderror
+                </div>
+            </div>
+
+            <div class="row mb-3">
+                <label for="github" class="col-sm-2 col-form-label">Github</label>
+                <div class="col-sm-10">
+                <input type="text" class="form-control @error('github') is-invalid @enderror" class="form-control" id="github">
+                @error('github')
+                    <span class="invalid-feedback" role="alert">
+                        <strong>{{ $message }}</strong>
+                    </span>
+                @enderror
+                </div>
+            </div> 
+            <div class="modal-footer">
+                <button type="button" class="btn btn-secondary" data-bs-dismiss="modal">Close</button>
+                <button type="submit" class="btn btn-primary">Save</button>
+            </div> 
+        </form>
+      </div>
+    </div>
+  </div>
+</div>
+<!-- ----------------------------------------------------------------------------------- -->
+
+<!-- -------------------------- Contact details model ---------------------------------- -->
+<div class="modal fade" id="contactstaticBackdrop" data-bs-backdrop="static" data-bs-keyboard="false" tabindex="-1" aria-labelledby="staticBackdropLabel" aria-hidden="true">
+  <div class="modal-dialog modal-dialog-centered">
+    <div class="modal-content">
+      <div class="modal-header">
+        <h5 class="modal-title" id="staticBackdropLabel">Edit Contact details</h5>
+        <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
+      </div>
+      <div class="modal-body">
+        <form method="POST" action="#" enctype="multipart/form-data">
+            @csrf
+            <div class="row mb-3">
+                <label for="address" class="col-sm-2 col-form-label">Address</label>
+                <div class="col-sm-10">
+                <input type="text" class="form-control @error('address') is-invalid @enderror" class="form-control" id="address">
+                @error('address')
+                    <span class="invalid-feedback" role="alert">
+                        <strong>{{ $message }}</strong>
+                    </span>
+                @enderror
+                </div>
+            </div>
+
+            <div class="row mb-3">
+                <label for="city" class="col-sm-2 col-form-label">City</label>
+                <div class="col-sm-10">
+                <input type="text" class="form-control @error('city') is-invalid @enderror" class="form-control" id="city">
+                @error('city')
+                    <span class="invalid-feedback" role="alert">
+                        <strong>{{ $message }}</strong>
+                    </span>
+                @enderror
+                </div>
+            </div> 
+
+            <div class="row mb-3">
+                <label for="province" class="col-sm-2 col-form-label">Province</label>
+                <div class="col-sm-10">
+                <input type="text" class="form-control @error('province') is-invalid @enderror" class="form-control" id="province">
+                @error('province')
+                    <span class="invalid-feedback" role="alert">
+                        <strong>{{ $message }}</strong>
+                    </span>
+                @enderror
+                </div>
+            </div>
+
+            <div class="row mb-3">
+                <label for="telNo" class="col-sm-2 col-form-label">Telephone No.</label>
+                <div class="col-sm-10">
+                <input type="text" class="form-control @error('telNo') is-invalid @enderror" class="form-control" id="telNo">
+                @error('telNo')
+                    <span class="invalid-feedback" role="alert">
+                        <strong>{{ $message }}</strong>
+                    </span>
+                @enderror
+                </div>
+            </div> 
+            <div class="modal-footer">
+                <button type="button" class="btn btn-secondary" data-bs-dismiss="modal">Close</button>
+                <button type="submit" class="btn btn-primary">Save</button>
+            </div> 
+        </form>
+      </div>
+    </div>
+  </div>
+</div>
+<!-- ----------------------------------------------------------------------------------- -->
+
+
+<!-- ------------------------ Profile view --------------------------------------------- -->
 <div class="container">
     <div class="bg-primary p-3 pb-1 rounded" style="--bs-bg-opacity: .1;">
         <nav aria-label="breadcrumb">
@@ -24,26 +193,32 @@
                         <h5 class="text-muted"> @chandula</h5>
                     </div>
                     <div class="d-flex flex-column border border-3 mt-3 p-3">
-                        <h4> Bio </h4>
+                        <div class="container p-0">
+                            <div class="float-start"><h4> Bio </h4></div>
+                            <div class="float-end"><a type="button" data-bs-toggle="modal" data-bs-target="#biostaticBackdrop" style="text-decoration: none; color:blue;"><i class="bi bi-pencil-square"></i> edit</a></div>
+                        </div>
                         <p>Lorem ipsum dolor, sit amet consectetur adipisicing elit. Laboriosam natus tenetur voluptas</p>
                     </div>
                 </div>
             </div>
 
             <div class="card mt-3">
-            <div class="card-header" style="font-weight: 1000"><i class="bi bi-messenger"></i>  Social Media</div>
+                <div class="card-header">
+                    <div class="float-start" style="font-weight: 1000"><i class="bi bi-messenger"></i>  Social Media</div>
+                    <div class="float-end"><a type="button" data-bs-toggle="modal" data-bs-target="#socialstaticBackdrop" style="text-decoration: none; color:blue;"><i class="bi bi-pencil-square"></i> edit</a></div>
+                </div>
                 <ul class="list-group list-group-flush">
                     <li class="list-group-item d-flex justify-content-between align-items-center flex-wrap">
-                        <h6 class="mb-0"> <i class="fas fa-address-card"></i> CV</h6>
-                        <span class="text-secondary"> <a href="https://drive.google.com/file/d/1dQP-suGSF7Svg9kKKyBuhXnQAifa3dSt/view?usp=sharing" target="_blank"><button type="button" class="btn btn-outline-primary btn-sm">View</button></a></span>
+                        <h6 class="mb-0">CV</h6>
+                        <span class="text-secondary"> <a href="#" target="_blank"><button type="button" class="btn btn-outline-secondary btn-sm">View</button></a></span>
                     </li>
                     <li class="list-group-item d-flex justify-content-between align-items-center flex-wrap">
-                        <h6 class="mb-0"> <i class="fab fa-linkedin"></i> LinkedIn</h6>
-                        <span class="text-secondary"> <a href="https://linkedin.com/in/isuri-devindi-b8602a206" target="_blank"><button type="button" class="btn btn-outline-primary btn-sm">View</button></a></span>
+                        <h6 class="mb-0">LinkedIn</h6>
+                        <span class="text-secondary"> <a href="#" target="_blank"><button type="button" class="btn btn-outline-secondary btn-sm">View</button></a></span>
                     </li>
                     <li class="list-group-item d-flex justify-content-between align-items-center flex-wrap">
-                        <h6 class="mb-0"> <i class="fab fa-github"></i> Github</h6>
-                        <span class="text-secondary"> <a href="https://github.com/Isuri-Devindi" target="_blank"><button type="button" class="btn btn-outline-primary btn-sm">View</button></a></span>
+                        <h6 class="mb-0">Github</h6>
+                        <span class="text-secondary"> <a href="#" target="_blank"><button type="button" class="btn btn-outline-secondary btn-sm">View</button></a></span>
                     </li>
                 </ul>
             </div>
@@ -51,7 +226,11 @@
     
         <div class="col-md-8">
             <div class="card mb-3">
-                <div class="card-header" style="font-weight: 1000"><i class="bi bi-person-fill"></i> Profile</div>
+                <div class="card-header">
+                    <div class="float-start" style="font-weight: 1000"><i class="bi bi-person-fill"></i> Profile</div>
+                    <div class="float-end"><a href="#" style="text-decoration: none;"><i class="bi bi-pencil-square"></i> edit</a></div>
+                </div>
+                
                 <div class="card-body">
                     <div class="row">
                         <div class="col-sm-3">
@@ -101,7 +280,10 @@
             </div>
 
             <div class="card mb-3">
-            <div class="card-header" style="font-weight: 1000"><i class="bi bi-person-lines-fill"></i>  Contact details</div>
+                <div class="card-header">
+                    <div class="float-start" style="font-weight: 1000"><i class="bi bi-person-lines-fill"></i>  Contact details</div>
+                    <div class="float-end"><a type="button" data-bs-toggle="modal" data-bs-target="#contactstaticBackdrop" style="text-decoration: none; color:blue;"><i class="bi bi-pencil-square"></i> edit</a></div>
+                </div>
             <div class="card-body">
                 <div class="row">
                     <div class="col-sm-3">
