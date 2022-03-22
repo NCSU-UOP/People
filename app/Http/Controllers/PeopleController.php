2f9bdd257c2221e4bd39b5e8d44b81c82217ff42
Isuru Lakshan <e16203@eng.pdn.ac.lk>
2022-03-23 00:12:50 +0530

    functions to redirect & view public profile

--- app/Http/Controllers/PeopleController.php
+++ app/Http/Controllers/PeopleController.php
@@ -3,9 +3,12 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
-use App\Models\faculty;
+use App\Models\Faculty;
+use App\Models\Department;
 use App\Models\Student;
+use App\Models\User;
 use App\Models\Batch;
+use Illuminate\Support\Arr;
 
 class PeopleController extends Controller
 {
@@ -26,10 +29,38 @@ class PeopleController extends Controller
         return view('people.student')->with('fac', $faculties)->with('batches', $batches);
     }
 
-    //profile view method(tempory)
-    public function getProfile()
+    //redirect to profile view route method
+    public function getProfile($facultyName,$batch,$id)
     {
-        return view('people.profile');
+        // dd($id);
+        $user = new User();
+        $username = $user::select('username')->where('id',$id)->first()->toArray();
+        // dd($username);
+        return redirect('uop/student/profile/'.$username['username']);
+    }
+
+    //profile view method
+    public function getProfileDetails($username)
+    {
+        // dd($username);
+        $user = new User();
+        $user = $user::select('id','email')->where('username',$username)->first()->toArray();
+        // dd($userid);
+        $student = new Student();
+        $studentdata = $student::where('id',$user['id'])->first()->toArray();
+        $faculty = new Faculty();
+        $department = new Department();
+        $facultyName = $faculty::where('id',$studentdata['faculty_id'])->first()->toArray();
+        $departmentName = $department::where('id',$studentdata['department_id'])->first()->toArray();
+        $date = date('d-m-Y',strtotime($studentdata['updated_at']));
+
+        $studentdata = Arr::add($studentdata,'facultyName',$facultyName['name']);
+        $studentdata = Arr::add($studentdata,'departmentName',$departmentName['name']);
+        $studentdata = Arr::add($studentdata,'username',$username);
+        $studentdata = Arr::add($studentdata,'email',$user['email']);
+        $studentdata = Arr::add($studentdata,'date',$date);
+        // dd($studentdata);
+        return view('people.profile')->with('student',$studentdata);
     }
 
     //getting student list method
@@ -44,7 +75,7 @@ class PeopleController extends Controller
         // $people = $person::select('image', 'fullname', 'regNo', 'username')->where('faculty_id', $fac_id)->where('batch_id', $batch)->orderBy('regNo', 'asc')->get();
 
         $student = new Student();
-        $studentList = $student::select('image', 'fullname', 'regNo')->where('faculty_id', $facultyData['id'])->where('batch_id', $batch)->where('is_verified', 1)->where('is_rejected', 0)->orderBy('regNo', 'asc')->get()->toArray();
+        $studentList = $student::select('id','image', 'fullname', 'regNo')->where('faculty_id', $facultyData['id'])->where('batch_id', $batch)->where('is_verified', 1)->where('is_rejected', 0)->orderBy('regNo', 'asc')->get()->toArray();
 
         // Change the image url to pick its respective thumbnails
         // foreach ($people as $key => $person) {
@@ -54,13 +85,13 @@ class PeopleController extends Controller
         // }
 
         // Change the image url to pick its respective thumbnails
-        foreach ($studentList as $key => $student) {
-            $image_link = explode('\\', $student->image);
-            $image_link[2] = 'thumbs';
-            $student->image = implode('/', $image_link);
-        }
+        // foreach ($studentList as $key => $student) {
+        //     $image_link = explode('\\', $student->image);
+        //     $image_link[2] = 'thumbs';
+        //     $student->image = implode('/', $image_link);
+        // }
 
         //dd($studentList);
-        return view('people.studentList')->with('facultyName', $facultyData['name'])->with('studentlist', $studentList)->with('batch', $batch)->with('facultyname', $fac_name);
+        return view('people.studentList')->with('facultyName', $facultyData['name'])->with('studentlist', $studentList)->with('batch', $batch);
     }
 }
