a0eb94d7b1ea3a4e0df33761d0f5aeb5e8fae3e8
Isuru Lakshan <e16203@eng.pdn.ac.lk>
2022-04-19 10:02:23 +0530

    bug fix

--- resources/views/vendor/LaravelLogger/scripts/confirm-modal.blade.php
+++ resources/views/vendor/LaravelLogger/scripts/confirm-modal.blade.php
@@ -1,17 +1,20 @@
 <script type="text/javascript">
 
+    $(function(){
+
     // Confirm Form Submit Modal
-    $('.{{$formTrigger}}').on('show.bs.modal', function (e) {
+    $("{{$formTrigger}}").on('show.bs.modal', function (e) {
         var message = $(e.relatedTarget).attr('data-message');
         var title = $(e.relatedTarget).attr('data-title');
         var form = $(e.relatedTarget).closest('form');
         $(this).find('.modal-body').text(message);
         $(this).find('.modal-title').text(title);
         $(this).find('.modal-footer #confirm').data('form', form);
-        console.log(e.relatedTarget);
     });
-    $('.{{$formTrigger}}').find('.modal-footer #confirm').on('click', function(){
+    $("{{$formTrigger}}").find('.modal-footer #confirm').on('click', function(){
         $(this).data('form').submit();
+    });
+
     });
 
 </script>
