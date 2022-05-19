66603caac512c72e231d4e2aa4360adeef79be9d
Isuru Lakshan <e16203@eng.pdn.ac.lk>
2022-05-19 11:12:31 +0530

    imports to user model

---
+++ app/Imports/UsersImport.php
@@ -0,0 +1,34 @@
+<?php
+
+namespace App\Imports;
+
+use App\Models\User;
+use Illuminate\Support\Facades\Hash;
+use Maatwebsite\Excel\Concerns\ToModel;
+use Maatwebsite\Excel\Concerns\WithHeadingRow;
+use Maatwebsite\Excel\Concerns\WithBatchInserts;
+use Maatwebsite\Excel\Concerns\WithUpserts;
+use Maatwebsite\Excel\Concerns\WithProgressBar;
+use Maatwebsite\Excel\Concerns\Importable;
+
+
+
+class UsersImport implements ToModel, WithHeadingRow, WithProgressBar
+{
+    use Importable;
+    /**
+    * @param array $row
+    *
+    * @return \Illuminate\Database\Eloquent\Model|null
+    */
+    public function model(array $row)
+    {
+        // dd($row);
+        
+        return new User([
+            'username' => $row['enrolment_no'],
+            'usertype' => $row['usertype'],
+            'password' => $row['nic'],
+        ]);
+    }
+}
