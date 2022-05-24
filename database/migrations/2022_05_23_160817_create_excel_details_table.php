9fb3e9256720ae25c0f986a6912c698022f05f15
Isuru Lakshan <e16203@eng.pdn.ac.lk>
2022-05-24 11:56:44 +0530

    add another column 'is_imported'

--- database/migrations/2022_05_23_160817_create_excel_details_table.php
+++ database/migrations/2022_05_23_160817_create_excel_details_table.php
@@ -22,6 +22,7 @@ return new class extends Migration
             $table->string('excel_filename', env("EXCEL_FILENAME_MAX", 20))->unique();
             $table->unsignedBigInteger('faculty_id');
             $table->integer('batch_id')->nullable();
+            $table->boolean('is_imported')->default(false);
             $table->unsignedBigInteger('department_id')->nullable();
             $table->string('excel_file_link')->unique();
             $table->timestamps();
