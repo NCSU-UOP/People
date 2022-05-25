01c44d8bc9530eb20678dbc97a69ee86578d226f
Isuru Lakshan <e16203@eng.pdn.ac.lk>
2022-05-26 00:57:42 +0530

    new controller functions added to load excel upload form and to upload excel files

--- app/Http/Controllers/ExcelFileController.php
+++ app/Http/Controllers/ExcelFileController.php
@@ -3,11 +3,33 @@
 namespace App\Http\Controllers;
 
 use App\Models\ExcelDetails;
+use App\Models\Faculty;
+use App\Models\Batch;
 
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Validator;
+use Illuminate\Validation\Rule;
 
 class ExcelFileController extends Controller
 {
+    protected $messages = [
+        'required' => 'The :attribute field is required.',
+        'same' => 'The :attribute and :other must match.',
+        'size' => 'The :attribute must be exactly :size.',
+        'min' => 'The :attribute must be greater than :min characters.',
+        'max' => 'The :attribute must be less than :max characters.',
+        'between' => 'The :attribute value :input is not between :min - :max.',
+        'in' => 'The :attribute must be one of the following types: :values',
+        'unique' => 'The :attribute is already in use.',
+        'exists' => 'The :attribute is invalid.',
+        'regex' => 'The :attribute format is invalid.',
+        'email' => 'Invalid email.',
+        'string' => 'The :attribute should be a string.',
+        'integer' => 'The :attribute field is required.',
+        'confirmed' => 'Password and Confirm Password must be match',
+        'mimes' => 'File format is invalid. only xlsx is supported.',
+    ];
+
     //function to import excel file, $id should be given from the route as a parameter
     public function importExcelFile($id)
     {   
@@ -48,5 +70,35 @@ class ExcelFileController extends Controller
     public function removeExcelFile($id)
     {   
 
+    }
+
+    // (Super Admin) add excel file
+    public function addExcelFile() 
+    {
+        $faculty = Faculty::select('id', 'name')->get();
+        $batch = Batch::select('id')->get();
+        $usertype = [[env('STUDENT'),'Student'], [env('ACADEMIC_STAFF'),'Academic Staff'], [env('NON_ACADEMIC_STAFF'),'Non-Academic Staff']];
+        // dd($usertype['1']);
+        return view('admin.uploadExcel', compact('faculty','batch','usertype'));
+    }
+
+    // upload excel file POST method
+    public function uploadExcelFile() 
+    {
+        // dd("hello");
+        $usertypes = [env('STUDENT'),env('ACADEMIC_STAFF'),env('NON_ACADEMIC_STAFF')];
+        $Data = request()->validate([
+            'usertype' => ['required','integer',Rule::in([env('STUDENT'),env('ACADEMIC_STAFF'),env('NON_ACADEMIC_STAFF')])],
+            'faculty_id' => ['required','int','exists:faculties,id'],
+            'batch_id' => ['int','exists:batches,id'],
+            'excel_file' => ['required', 'file', 'mimes:xlsx,xls', 'max:2048'],
+            'excelAttributes' => ['required', 'array'],
+        ], $this->messages);
+
+        dd($Data);
+        //!TODO should implement storing the excel file in public folder and storing the details in the database
+        // dd($adminData);
+
+        // return redirect('/dashboard')->with('message', 'User has been created Succesfully 👍');
     }
 }
